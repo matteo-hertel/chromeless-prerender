9265d144663dd2c30cdae0220e47c3f7a4efdff3
Matteo Hertel <info@matteohertel.com>
2017-08-18 16:25:03 +0200

    add 5 mins cache to fetchHTML endpoint

--- code/controllers/chromeless/index.js
+++ code/controllers/chromeless/index.js
@@ -1,9 +1,10 @@
 const { fetchHTML } = require(`${__dirname}/../../modules/chromelessWrapper`);
 const acceptValidDomains = require(`${__dirname}/../../middlewares/acceptValidDomains`)
+const cache = require(`${__dirname}/../../middlewares/inMemoryCache`)
 module.exports = (router) => {
 
-    router.get('/api/v0.1/fetchHTML', acceptValidDomains, (req, res) => {
-
+    router.get('/api/v0.1/fetchHTML', [acceptValidDomains, cache(300)], (req, res) => {
+        console.log();
         fetchHTML(req.urlToFetch)
             .then(html => res.send(html))
             .catch(err => {
